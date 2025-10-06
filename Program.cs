790b795d9422ac018aa705338ee4299a939171ad
23011556-035-commits <23011556-035@uog.edu.pk>
2025-10-06 21:29:41 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+namespace _23011556_035
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            evenoddchecker q1 = new evenoddchecker();
+            SimpleCalculator q2  = new SimpleCalculator();
+            GradeEvaluator q3 = new GradeEvaluator();
+            SumOfNaturalNumbers q4 = new SumOfNaturalNumbers();
+            MultiplicationTable q5 = new MultiplicationTable();
+            FactorialCalculator q6 = new FactorialCalculator();
+            ReverseNumber q7 = new ReverseNumber();
+            ArrayMaxMin q8 = new ArrayMaxMin();
+            ArrayEvenOddCount q9 = new ArrayEvenOddCount();
+            ArraySearch q10 = new ArraySearch();
+         
+
+        }
+    }
+}
+
+
