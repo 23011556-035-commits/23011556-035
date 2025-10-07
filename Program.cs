232be850d94dab630b397fe52095a1785a598cb3
23011556-035-commits <23011556-035@uog.edu.pk>
2025-10-07 20:02:26 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,20 +1,42 @@
 using System;
 namespace _23011556_035
 {
-    class Program
+   internal class Program
     {
         static void Main(string[] args)
         {
-            evenoddchecker q1 = new evenoddchecker();
-            SimpleCalculator q2  = new SimpleCalculator();
-            GradeEvaluator q3 = new GradeEvaluator();
-            SumOfNaturalNumbers q4 = new SumOfNaturalNumbers();
-            MultiplicationTable q5 = new MultiplicationTable();
-            FactorialCalculator q6 = new FactorialCalculator();
-            ReverseNumber q7 = new ReverseNumber();
-            ArrayMaxMin q8 = new ArrayMaxMin();
-            ArrayEvenOddCount q9 = new ArrayEvenOddCount();
-            ArraySearch q10 = new ArraySearch();
+    
+         ArrayEvenOddCount q1 = new ArrayEvenOddCount();
+            q1.EvenOddCount();
+
+            ArrayMaxMin q2= new ArrayMaxMin();
+            q2.Array();
+
+            ArraySearch q3= new ArraySearch();
+            q3.Array1();
+
+            evenoddchecker q4= new evenoddchecker();
+            q4.EvenOdd();
+
+            FactorialCalculator q5= new FactorialCalculator();
+            q5.Factorial();
+
+            GradeEvaluator q6= new GradeEvaluator();
+            q6.Grade();
+
+            MultiplicationTable q7= new MultiplicationTable();
+            q7.Multiplication();
+
+            ReverseNumber q8 = new ReverseNumber();
+            q8.Reverse();
+
+            SimpleCalculator q9 = new SimpleCalculator();
+            q9.Calculator();
+
+            SumOfNaturalNumbers q10 = new SumOfNaturalNumbers();
+            q10.NaturalNumbers();
+        
+
          
 
         }
